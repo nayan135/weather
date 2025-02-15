4d387f555abc19265c9fa46169265737e1a6b90e
NAYAN ACHARYA <acharyanayan604@gmail.com>
2025-02-15 18:45:04 +0545

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const buttonEl = document.getElementById('button');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
+const API_KEY ='025da457421ee005e1a9982da52aaf31';
 
 
 setInterval(() => {
